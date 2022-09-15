9aa1e7aa7f3d721bdd485afedffaf1b7442f2e4c
ihgumilar <igum002@aucklanduni.ac.nz>
2022-09-15 15:11:52 +1200

    Fixed file order and naming even subjects
    * Previously, the order of files to be combined similar with odd subject
    * Similarly, for file naming, it was similar to odd subjects.
    * For example, odd subject : pre = right_left, post = left_right
                   even subject : pre = left_right , post = right_left

--- EEG/pre-processing/combine_fif_files.py
+++ EEG/pre-processing/combine_fif_files.py
@@ -17,6 +17,8 @@
 # %% [markdown]
 # ## Relevant packages
 
+import os
+
 # %%
 import mne
 from tqdm import tqdm
@@ -75,15 +77,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 1) + "-averted_post_left_point_raw.fif", verbose=False
     )
     averted_post_files_to_combine = [
-        averted_post_right_odd_subject,
         averted_post_left_odd_subject,
+        averted_post_right_odd_subject,
     ]
     combined_post_averted_files = mne.concatenate_raws(averted_post_files_to_combine)
     combined_post_averted_files_label = (
         combined_experimental_dir
         + "S0"
         + str(i + 1)
-        + "-averted_post_right_left_point_combined_raw.fif"
+        + "-averted_post_left_right_point_combined_raw.fif"
     )
     combined_post_averted_files.save(combined_post_averted_files_label, overwrite=True)
 
@@ -115,15 +117,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 1) + "-direct_post_left_point_raw.fif", verbose=False
     )
     direct_post_files_to_combine = [
-        direct_post_right_odd_subject,
         direct_post_left_odd_subject,
+        direct_post_right_odd_subject,
     ]
     combined_post_direct_files = mne.concatenate_raws(direct_post_files_to_combine)
     combined_post_direct_files_label = (
         combined_experimental_dir
         + "S0"
         + str(i + 1)
-        + "-direct_post_right_left_point_combined_raw.fif"
+        + "-direct_post_left_right_point_combined_raw.fif"
     )
     combined_post_direct_files.save(combined_post_direct_files_label, overwrite=True)
 
@@ -155,15 +157,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 1) + "-natural_post_left_point_raw.fif", verbose=False
     )
     natural_post_files_to_combine = [
-        natural_post_right_odd_subject,
         natural_post_left_odd_subject,
+        natural_post_right_odd_subject,
     ]
     combined_post_natural_files = mne.concatenate_raws(natural_post_files_to_combine)
     combined_post_natural_files_label = (
         combined_experimental_dir
         + "S0"
         + str(i + 1)
-        + "-natural_post_right_left_point_combined_raw.fif"
+        + "-natural_post_left_right_point_combined_raw.fif"
     )
     combined_post_natural_files.save(combined_post_natural_files_label, overwrite=True)
 
@@ -188,15 +190,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 2) + "-averted_pre_left_point_raw.fif", verbose=False
     )
     averted_pre_files_to_combine = [
-        averted_pre_right_odd_subject,
         averted_pre_left_odd_subject,
+        averted_pre_right_odd_subject,
     ]
     combined_pre_averted_files = mne.concatenate_raws(averted_pre_files_to_combine)
     combined_pre_averted_files_label = (
         combined_experimental_dir
         + "S0"
         + str(i + 2)
-        + "-averted_pre_right_left_point_combined_raw.fif"
+        + "-averted_pre_left_right_point_combined_raw.fif"
     )
     combined_pre_averted_files.save(combined_pre_averted_files_label, overwrite=True)
 
@@ -228,15 +230,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 2) + "-direct_pre_left_point_raw.fif", verbose=False
     )
     direct_pre_files_to_combine = [
-        direct_pre_right_odd_subject,
         direct_pre_left_odd_subject,
+        direct_pre_right_odd_subject,
     ]
     combined_pre_direct_files = mne.concatenate_raws(direct_pre_files_to_combine)
     combined_pre_direct_files_label = (
         combined_experimental_dir
         + "S0"
         + str(i + 2)
-        + "-direct_pre_right_left_point_combined_raw.fif"
+        + "-direct_pre_left_right_point_combined_raw.fif"
     )
     combined_pre_direct_files.save(combined_pre_direct_files_label, overwrite=True)
 
@@ -268,15 +270,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 2) + "-natural_pre_left_point_raw.fif", verbose=False
     )
     natural_pre_files_to_combine = [
-        natural_pre_right_odd_subject,
         natural_pre_left_odd_subject,
+        natural_pre_right_odd_subject,
     ]
     combined_pre_natural_files = mne.concatenate_raws(natural_pre_files_to_combine)
     combined_pre_natural_files_label = (
         combined_experimental_dir
         + "S0"
         + str(i + 2)
-        + "-natural_pre_right_left_point_combined_raw.fif"
+        + "-natural_pre_left_right_point_combined_raw.fif"
     )
     combined_pre_natural_files.save(combined_pre_natural_files_label, overwrite=True)
 
@@ -323,15 +325,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i) + "-averted_pre_left_point_raw.fif", verbose=False
         )
         averted_pre_files_to_combine = [
-            averted_pre_right_odd_subject,
             averted_pre_left_odd_subject,
+            averted_pre_right_odd_subject,
         ]
         combined_pre_averted_files = mne.concatenate_raws(averted_pre_files_to_combine)
         combined_pre_averted_files_label = (
             combined_experimental_dir
             + "S"
             + str(i)
-            + "-averted_pre_right_left_point_combined_raw.fif"
+            + "-averted_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_averted_files.save(
             combined_pre_averted_files_label, overwrite=True
@@ -369,15 +371,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i) + "-direct_pre_left_point_raw.fif", verbose=False
         )
         direct_pre_files_to_combine = [
-            direct_pre_right_odd_subject,
             direct_pre_left_odd_subject,
+            direct_pre_right_odd_subject,
         ]
         combined_pre_direct_files = mne.concatenate_raws(direct_pre_files_to_combine)
         combined_pre_direct_files_label = (
             combined_experimental_dir
             + "S"
             + str(i)
-            + "-direct_pre_right_left_point_combined_raw.fif"
+            + "-direct_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_direct_files.save(combined_pre_direct_files_label, overwrite=True)
 
@@ -411,15 +413,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i) + "-natural_pre_left_point_raw.fif", verbose=False
         )
         natural_pre_files_to_combine = [
-            natural_pre_right_odd_subject,
             natural_pre_left_odd_subject,
+            natural_pre_right_odd_subject,
         ]
         combined_pre_natural_files = mne.concatenate_raws(natural_pre_files_to_combine)
         combined_pre_natural_files_label = (
             combined_experimental_dir
             + "S"
             + str(i)
-            + "-natural_pre_right_left_point_combined_raw.fif"
+            + "-natural_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_natural_files.save(
             combined_pre_natural_files_label, overwrite=True
@@ -459,15 +461,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i + 2) + "-averted_pre_left_point_raw.fif", verbose=False
         )
         averted_pre_files_to_combine = [
-            averted_pre_right_odd_subject,
             averted_pre_left_odd_subject,
+            averted_pre_right_odd_subject,
         ]
         combined_pre_averted_files = mne.concatenate_raws(averted_pre_files_to_combine)
         combined_pre_averted_files_label = (
             combined_experimental_dir
             + "S"
             + str(i + 2)
-            + "-averted_pre_right_left_point_combined_raw.fif"
+            + "-averted_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_averted_files.save(
             combined_pre_averted_files_label, overwrite=True
@@ -505,15 +507,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i + 2) + "-direct_pre_left_point_raw.fif", verbose=False
         )
         direct_pre_files_to_combine = [
-            direct_pre_right_odd_subject,
             direct_pre_left_odd_subject,
+            direct_pre_right_odd_subject,
         ]
         combined_pre_direct_files = mne.concatenate_raws(direct_pre_files_to_combine)
         combined_pre_direct_files_label = (
             combined_experimental_dir
             + "S"
             + str(i + 2)
-            + "-direct_pre_right_left_point_combined_raw.fif"
+            + "-direct_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_direct_files.save(combined_pre_direct_files_label, overwrite=True)
 
@@ -547,15 +549,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i + 2) + "-natural_pre_left_point_raw.fif", verbose=False
         )
         natural_pre_files_to_combine = [
-            natural_pre_right_odd_subject,
             natural_pre_left_odd_subject,
+            natural_pre_right_odd_subject,
         ]
         combined_pre_natural_files = mne.concatenate_raws(natural_pre_files_to_combine)
         combined_pre_natural_files_label = (
             combined_experimental_dir
             + "S"
             + str(i + 2)
-            + "-natural_pre_right_left_point_combined_raw.fif"
+            + "-natural_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_natural_files.save(
             combined_pre_natural_files_label, overwrite=True
@@ -626,15 +628,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S" + str(i + 1) + "-averted_post_left_point_raw.fif", verbose=False
     )
     averted_post_files_to_combine = [
-        averted_post_right_odd_subject,
         averted_post_left_odd_subject,
+        averted_post_right_odd_subject,
     ]
     combined_post_averted_files = mne.concatenate_raws(averted_post_files_to_combine)
     combined_post_averted_files_label = (
         combined_experimental_dir
         + "S"
         + str(i + 1)
-        + "-averted_post_right_left_point_combined_raw.fif"
+        + "-averted_post_left_right_point_combined_raw.fif"
     )
     combined_post_averted_files.save(combined_post_averted_files_label, overwrite=True)
 
@@ -666,15 +668,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S" + str(i + 1) + "-direct_post_left_point_raw.fif", verbose=False
     )
     direct_post_files_to_combine = [
-        direct_post_right_odd_subject,
         direct_post_left_odd_subject,
+        direct_post_right_odd_subject,
     ]
     combined_post_direct_files = mne.concatenate_raws(direct_post_files_to_combine)
     combined_post_direct_files_label = (
         combined_experimental_dir
         + "S"
         + str(i + 1)
-        + "-direct_post_right_left_point_combined_raw.fif"
+        + "-direct_post_left_right_point_combined_raw.fif"
     )
     combined_post_direct_files.save(combined_post_direct_files_label, overwrite=True)
 
@@ -706,15 +708,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S" + str(i + 1) + "-natural_post_left_point_raw.fif", verbose=False
     )
     natural_post_files_to_combine = [
-        natural_post_right_odd_subject,
         natural_post_left_odd_subject,
+        natural_post_right_odd_subject,
     ]
     combined_post_natural_files = mne.concatenate_raws(natural_post_files_to_combine)
     combined_post_natural_files_label = (
         combined_experimental_dir
         + "S"
         + str(i + 1)
-        + "-natural_post_right_left_point_combined_raw.fif"
+        + "-natural_post_left_right_point_combined_raw.fif"
     )
     combined_post_natural_files.save(combined_post_natural_files_label, overwrite=True)
 
@@ -775,15 +777,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 1) + "-averted_post_left_point_raw.fif", verbose=False
     )
     averted_post_files_to_combine = [
-        averted_post_right_odd_subject,
         averted_post_left_odd_subject,
+        averted_post_right_odd_subject,
     ]
     combined_post_averted_files = mne.concatenate_raws(averted_post_files_to_combine)
     combined_post_averted_files_label = (
         combined_baseline_dir
         + "S0"
         + str(i + 1)
-        + "-averted_post_right_left_point_combined_raw.fif"
+        + "-averted_post_left_right_point_combined_raw.fif"
     )
     combined_post_averted_files.save(combined_post_averted_files_label, overwrite=True)
 
@@ -807,15 +809,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S0" + str(i + 2) + "-averted_pre_left_point_raw.fif", verbose=False
     )
     averted_pre_files_to_combine = [
-        averted_pre_right_odd_subject,
         averted_pre_left_odd_subject,
+        averted_pre_right_odd_subject,
     ]
     combined_pre_averted_files = mne.concatenate_raws(averted_pre_files_to_combine)
     combined_pre_averted_files_label = (
         combined_baseline_dir
         + "S0"
         + str(i + 2)
-        + "-averted_pre_right_left_point_combined_raw.fif"
+        + "-averted_pre_left_right_point_combined_raw.fif"
     )
     combined_pre_averted_files.save(combined_pre_averted_files_label, overwrite=True)
 
@@ -861,15 +863,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i) + "-averted_pre_left_point_raw.fif", verbose=False
         )
         averted_pre_files_to_combine = [
-            averted_pre_right_odd_subject,
             averted_pre_left_odd_subject,
+            averted_pre_right_odd_subject,
         ]
         combined_pre_averted_files = mne.concatenate_raws(averted_pre_files_to_combine)
         combined_pre_averted_files_label = (
             combined_baseline_dir
             + "S"
             + str(i)
-            + "-averted_pre_right_left_point_combined_raw.fif"
+            + "-averted_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_averted_files.save(
             combined_pre_averted_files_label, overwrite=True
@@ -909,15 +911,15 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
             "EEG-S" + str(i + 2) + "-averted_pre_left_point_raw.fif", verbose=False
         )
         averted_pre_files_to_combine = [
-            averted_pre_right_odd_subject,
             averted_pre_left_odd_subject,
+            averted_pre_right_odd_subject,
         ]
         combined_pre_averted_files = mne.concatenate_raws(averted_pre_files_to_combine)
         combined_pre_averted_files_label = (
             combined_baseline_dir
             + "S"
             + str(i + 2)
-            + "-averted_pre_right_left_point_combined_raw.fif"
+            + "-averted_pre_left_right_point_combined_raw.fif"
         )
         combined_pre_averted_files.save(
             combined_pre_averted_files_label, overwrite=True
@@ -987,16 +989,18 @@ for i in tqdm(range(begin, end, step), desc="Just relax and drink your coffee.."
         "EEG-S" + str(i + 1) + "-averted_post_left_point_raw.fif", verbose=False
     )
     averted_post_files_to_combine = [
-        averted_post_right_odd_subject,
         averted_post_left_odd_subject,
+        averted_post_right_odd_subject,
     ]
     combined_post_averted_files = mne.concatenate_raws(averted_post_files_to_combine)
     combined_post_averted_files_label = (
         combined_baseline_dir
         + "S"
         + str(i + 1)
-        + "-averted_post_right_left_point_combined_raw.fif"
+        + "-averted_post_left_right_point_combined_raw.fif"
     )
     combined_post_averted_files.save(combined_post_averted_files_label, overwrite=True)
 
 print("You files have combined, sir !. Just continue your coffee :)")
+
+# %%
