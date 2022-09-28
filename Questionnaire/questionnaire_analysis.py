856b6c6c7ada98b5aae5d1b0cb7ff437a2c549e6
ihgumilar <igum002@aucklanduni.ac.nz>
2022-09-28 23:25:44 +1300

    Compare total score of SPGQ pre vs post
     * It also compares total score of CoPresence
     ToDo :
       * Need to add standard deviation score for each t-test
       * T-test between subscale of SPGQ (pre vs post)
       * Correlate CoPresense and SPGQ
       * Correlate CoPresence and subscales of SPGQ

--- Questionnaire/questionnaire_analysis.py
+++ Questionnaire/questionnaire_analysis.py
@@ -23,6 +23,8 @@ import os
 import numpy as np
 import pandas as pd
 
+from scipy.stats import ttest_rel
+
 # %% [markdown]
 # ### List files in questionnaire folder
 
@@ -131,8 +133,307 @@ for idx, file in enumerate(averted_pre_questions):
 # Combine int a single dataframe for averted pre
 df_averted_pre = pd.concat(averted_pre_all_data_list, ignore_index=True)
 
-df_averted_pre
+df_averted_pre.head()
 
 
     
 
+
+# %% [markdown]
+# ### Scoring for each subscale (averted_post)
+# - Combine into a single dataframe
+
+# %%
+# ### Load questrionnaire data
+
+averted_post_all_data_list = []
+# TODO Create a loop that takes all files from the above list
+for idx, file in enumerate(averted_post_questions):
+
+    file_to_load = questionnaire_folder + file
+
+    df = pd.read_csv(file_to_load, sep=";",)
+
+    # Sum subscore of empathy
+    df["Empathy SPGQ"] = df["Answer"][:7].sum()
+
+    # Sum subscore of negative feeling
+    df["NegativeFeelings SPGQ"] = df["Answer"][7:13].sum()
+
+    # Sum subscore of behavioral
+    df["Behavioural SPGQ"] = df["Answer"][13:21].sum()
+
+    # Total score of SPGQ
+    subscales_spgq = ["Empathy SPGQ", "NegativeFeelings SPGQ", "Behavioural SPGQ"]
+    df["SPGQ Total"] = df[subscales_spgq].sum(axis=1)
+
+    # Total score of copresence
+    df["CoPresence Total"] = df["Answer"][7:13].sum()
+
+
+    # Get first row and all columns
+    df_clean = df.iloc[0:1, 4:]
+
+    # Put into a list
+    averted_post_all_data_list.append(df_clean)
+
+# Combine int a single dataframe for averted pre
+df_averted_post = pd.concat(averted_post_all_data_list, ignore_index=True)
+
+df_averted_post.head()
+
+    
+
+# %% [markdown]
+# ### Scoring for each subscale (direct_pre)
+# - Combine into a single dataframe
+
+# %%
+# ### Load questrionnaire data
+
+direct_pre_all_data_list = []
+# TODO Create a loop that takes all files from the above list
+for idx, file in enumerate(direct_pre_questions):
+
+    file_to_load = questionnaire_folder + file
+
+    df = pd.read_csv(file_to_load, sep=";",)
+
+    # Sum subscore of empathy
+    df["Empathy SPGQ"] = df["Answer"][:7].sum()
+
+    # Sum subscore of negative feeling
+    df["NegativeFeelings SPGQ"] = df["Answer"][7:13].sum()
+
+    # Sum subscore of behavioral
+    df["Behavioural SPGQ"] = df["Answer"][13:21].sum()
+
+    # Total score of SPGQ
+    subscales_spgq = ["Empathy SPGQ", "NegativeFeelings SPGQ", "Behavioural SPGQ"]
+    df["SPGQ Total"] = df[subscales_spgq].sum(axis=1)
+
+    # Total score of copresence
+    df["CoPresence Total"] = df["Answer"][7:13].sum()
+
+
+    # Get first row and all columns
+    df_clean = df.iloc[0:1, 4:]
+
+    # Put into a list
+    direct_pre_all_data_list.append(df_clean)
+
+# Combine int a single dataframe for averted pre
+df_direct_pre = pd.concat(direct_pre_all_data_list, ignore_index=True)
+
+df_direct_pre.head()
+
+    
+
+# %% [markdown]
+# ### Scoring for each subscale (direct_post)
+# - Combine into a single dataframe
+
+# %%
+# ### Load questrionnaire data
+
+direct_post_all_data_list = []
+# TODO Create a loop that takes all files from the above list
+for idx, file in enumerate(direct_post_questions):
+
+    file_to_load = questionnaire_folder + file
+
+    df = pd.read_csv(file_to_load, sep=";",)
+
+    # Sum subscore of empathy
+    df["Empathy SPGQ"] = df["Answer"][:7].sum()
+
+    # Sum subscore of negative feeling
+    df["NegativeFeelings SPGQ"] = df["Answer"][7:13].sum()
+
+    # Sum subscore of behavioral
+    df["Behavioural SPGQ"] = df["Answer"][13:21].sum()
+
+    # Total score of SPGQ
+    subscales_spgq = ["Empathy SPGQ", "NegativeFeelings SPGQ", "Behavioural SPGQ"]
+    df["SPGQ Total"] = df[subscales_spgq].sum(axis=1)
+
+    # Total score of copresence
+    df["CoPresence Total"] = df["Answer"][7:13].sum()
+
+
+    # Get first row and all columns
+    df_clean = df.iloc[0:1, 4:]
+
+    # Put into a list
+    direct_post_all_data_list.append(df_clean)
+
+# Combine int a single dataframe for averted pre
+df_direct_post = pd.concat(direct_post_all_data_list, ignore_index=True)
+
+df_direct_post.head()
+
+    
+
+# %% [markdown]
+# ### Scoring for each subscale (natural_pre)
+# - Combine into a single dataframe
+
+# %%
+# ### Load questrionnaire data
+
+natural_pre_all_data_list = []
+# TODO Create a loop that takes all files from the above list
+for idx, file in enumerate(natural_pre_questions):
+
+    file_to_load = questionnaire_folder + file
+
+    df = pd.read_csv(file_to_load, sep=";",)
+
+    # Sum subscore of empathy
+    df["Empathy SPGQ"] = df["Answer"][:7].sum()
+
+    # Sum subscore of negative feeling
+    df["NegativeFeelings SPGQ"] = df["Answer"][7:13].sum()
+
+    # Sum subscore of behavioral
+    df["Behavioural SPGQ"] = df["Answer"][13:21].sum()
+
+    # Total score of SPGQ
+    subscales_spgq = ["Empathy SPGQ", "NegativeFeelings SPGQ", "Behavioural SPGQ"]
+    df["SPGQ Total"] = df[subscales_spgq].sum(axis=1)
+
+    # Total score of copresence
+    df["CoPresence Total"] = df["Answer"][7:13].sum()
+
+
+    # Get first row and all columns
+    df_clean = df.iloc[0:1, 4:]
+
+    # Put into a list
+    natural_pre_all_data_list.append(df_clean)
+
+# Combine int a single dataframe for averted pre
+df_natural_pre = pd.concat(natural_pre_all_data_list, ignore_index=True)
+
+df_natural_pre.head()
+
+    
+
+# %% [markdown]
+# ### Scoring for each subscale (natural_post)
+# - Combine into a single dataframe
+
+# %%
+# ### Load questrionnaire data
+
+natural_post_all_data_list = []
+# TODO Create a loop that takes all files from the above list
+for idx, file in enumerate(natural_post_questions):
+
+    file_to_load = questionnaire_folder + file
+
+    df = pd.read_csv(file_to_load, sep=";",)
+
+    # Sum subscore of empathy
+    df["Empathy SPGQ"] = df["Answer"][:7].sum()
+
+    # Sum subscore of negative feeling
+    df["NegativeFeelings SPGQ"] = df["Answer"][7:13].sum()
+
+    # Sum subscore of behavioral
+    df["Behavioural SPGQ"] = df["Answer"][13:21].sum()
+
+    # Total score of SPGQ
+    subscales_spgq = ["Empathy SPGQ", "NegativeFeelings SPGQ", "Behavioural SPGQ"]
+    df["SPGQ Total"] = df[subscales_spgq].sum(axis=1)
+
+    # Total score of copresence
+    df["CoPresence Total"] = df["Answer"][7:13].sum()
+
+
+    # Get first row and all columns
+    df_clean = df.iloc[0:1, 4:]
+
+    # Put into a list
+    natural_post_all_data_list.append(df_clean)
+
+# Combine int a single dataframe for averted pre
+df_natural_post = pd.concat(natural_post_all_data_list, ignore_index=True)
+
+df_natural_post.head()
+
+    
+
+# %% [markdown]
+# ### SPGQ T-test averted pre vs averted post (Sig*)
+
+# %%
+spgq_averted_pre = df_averted_pre["SPGQ Total"]
+spgq_averted_post = df_averted_post["SPGQ Total"]
+result_averted_spgq_pre_vs_spgq_post = ttest_rel(spgq_averted_pre, spgq_averted_post)
+
+print(f"means of SPGQ averted_pre : {np.mean(spgq_averted_pre)}")
+print(f"means of SPGQ averted_post : {np.mean(spgq_averted_post)}")
+print(result_averted_spgq_pre_vs_spgq_post)
+
+# %% [markdown]
+# ### SPGQ T-test direct pre vs direct post (Sig*)
+
+# %%
+spgq_direct_pre = df_direct_pre["SPGQ Total"]
+spgq_direct_post = df_direct_post["SPGQ Total"]
+result_direct_spgq_pre_vs_spgq_post = ttest_rel(spgq_direct_pre, spgq_direct_post)
+
+print(f"means of SPGQ direct_pre : {np.mean(spgq_direct_pre)}")
+print(f"means of SPGQ direct_post : {np.mean(spgq_direct_post)}")
+print(result_direct_spgq_pre_vs_spgq_post)
+
+# %% [markdown]
+# ### SPGQ T-test natural pre vs natural post (Sig*)
+
+# %%
+spgq_natural_pre = df_natural_pre["SPGQ Total"]
+spgq_natural_post = df_natural_post["SPGQ Total"]
+result_direct_spgq_pre_vs_spgq_post = ttest_rel(spgq_natural_pre, spgq_natural_post)
+std_natural_pre = df_natural_pre["SPGQ Total"].std()
+std_natural_post = df_natural_post["SPGQ Total"].std()
+
+print(f"means of SPGQ natural_pre : {np.mean(spgq_natural_pre)}, SD = {std_natural_pre}")
+print(f"means of SPGQ natural_post : {np.mean(spgq_natural_post)}, SD = {std_natural_post}")
+print(result_direct_spgq_pre_vs_spgq_post)
+
+# %% [markdown]
+# ### CoPresence T-test averted pre vs averted post
+
+# %%
+copresence_averted_pre = df_averted_pre["CoPresence Total"]
+copresence_averted_post = df_averted_post["CoPresence Total"]
+result_averted_copresence_pre_vs_copresence_post = ttest_rel(copresence_averted_pre, copresence_averted_post)
+
+print(f"means of CoPresence averted_pre : {np.mean(copresence_averted_pre)}")
+print(f"means of CoPresence averted_post : {np.mean(copresence_averted_post)}")
+print(result_averted_copresence_pre_vs_copresence_post)
+
+# %% [markdown]
+# ### CoPresence T-test direct pre vs direct post
+
+# %%
+copresence_direct_pre = df_direct_pre["CoPresence Total"]
+copresence_direct_post = df_direct_post["CoPresence Total"]
+result_direct_copresence_pre_vs_copresence_post = ttest_rel(copresence_direct_pre, copresence_direct_post)
+
+print(f"means of CoPresence direct_pre : {np.mean(copresence_direct_pre)}")
+print(f"means of CoPresence direct_post : {np.mean(copresence_direct_post)}")
+print(result_direct_copresence_pre_vs_copresence_post)
+
+# %% [markdown]
+# ### CoPresence T-test natural pre vs natural post (Sig*)
+
+# %%
+copresence_natural_pre = df_natural_pre["CoPresence Total"]
+copresence_natural_post = df_natural_post["CoPresence Total"]
+result_natural_copresence_pre_vs_copresence_post = ttest_rel(copresence_natural_pre, copresence_natural_post)
+
+print(f"means of CoPresence natural_pre : {np.mean(copresence_natural_pre)}")
+print(f"means of CoPresence natural_post : {np.mean(copresence_natural_post)}")
+print(result_natural_copresence_pre_vs_copresence_post)
