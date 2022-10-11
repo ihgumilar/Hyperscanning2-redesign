cc945cb9421ddfbb51cd84e73c7ea0408bb4cc24
ihgumilar <igum002@aucklanduni.ac.nz>
2022-10-11 15:58:03 +1300

    Add function to add leading zero
     * It also replaces actual file names
     * This will apply to subject no. 1 - 9
     * For all algorithm : ccorr, coh, and plv

--- EEG/analysis/eeg_and_questionnaire_analysis.py
+++ EEG/analysis/eeg_and_questionnaire_analysis.py
@@ -73,6 +73,7 @@
 # %%
 import os 
 import re
+import pathlib
 import pandas as pd
 
 
@@ -186,264 +187,134 @@ print(total3)
 
 
 # %% [markdown]
-# ## Getting all files in a folder and filter only actual score
+# ## Add leading zeros for subject number 1 - 9 
+# For all algorithms : ccorr, coh, and plv
 
 # %%
+def add_leading_zero(path,N):
+    # N = No. of zeros required
+    files = os.listdir(path)
+    new_filenames = []
+    files_to_rename = []
 
-directory = "/hpc/igum002/codes/Hyperscanning2-redesign/data/EEG/significant_connections/averted_pre_copy"
-files = os.listdir(directory) 
+    # Loop the filename
+    for file in files:
 
+        # Find index "S" and get 2 characters after "S"
+        s_idx = file.index("S")
+        subj_no = file[s_idx+1 : s_idx+3]
 
-# %%
+        # Check if the 2nd character is digit or not
+        # If digit, don't include. Because we want only subject number less than 10 (satuan in indonesian language :)
+        if (subj_no[-1]>="0" and subj_no[-1]<="9"):
+            pass
 
-new_plv_filename = []
-files_to_rename = []
+        # If not digit, then INCLUDE ! and we want only score data 
+        else : 
+            if ("actual_score_data" in file):
+                # This populate all subject with no. less than 11
+                files_to_rename.append(file)
 
-# Loop the filename
-for file in files:
-
-    # Find index "S" and get 2 characters after "S"
-    s_idx = file.index("S")
-    subj_no = file[s_idx+1 : s_idx+3]
-
-    # Check if the 2nd character is digit or not
-    # If digit, don't include. Because we want only subject no less than 10 (satuan in indonesian language :)
-    if (subj_no[-1]>="0" and subj_no[-1]<="9"):
-        pass
-
-    # If not digit, then INCLUDE ! and we want only score data 
-    else : 
-        if ("actual_score_data" in file):
-            # This populate all subject with no. less than 11
-            files_to_rename.append(file)
-
-  
-    
-for idx in range(0,len(files_to_rename),5):
-    if (idx == 0):
-        files_per_algorithm = files_to_rename[idx:idx+5]
-        # print(files_per_algorithm)
-    elif (idx == 5):
-        files_per_algorithm = files_to_rename[idx:idx+5]
-        # print(files_per_algorithm)
-    elif (idx == 10):
-        files_per_algorithm = files_to_rename[idx:idx+5]
-        # print(files_per_algorithm)
-    
-    
-    for idx, file_per_algorithm in enumerate(files_per_algorithm):
-        if (idx==0):
-            idx1 = file_per_algorithm.index(str(idx+1))
-            idx2 = file_per_algorithm.index(str(idx+2))
-
-        elif (idx==1):
-            idx1 = file_per_algorithm.index(str(idx+2))
-            idx2 = file_per_algorithm.index(str(idx+3))
-
-        elif (idx==2):
-            idx1 = file_per_algorithm.index(str(idx+3))
-            idx2 = file_per_algorithm.index(str(idx+4))
         
-        elif (idx==3):
-            idx1 = file_per_algorithm.index(str(idx+4))
-            idx2 = file_per_algorithm.index(str(idx+5))
-
-        elif (idx==4):
-            idx1 = file_per_algorithm.index(str(idx+5))
-            idx2 = file_per_algorithm.index(str(idx+6))
-
-
-        subj_no_1 = file_per_algorithm[idx1:idx1+1]
-        if (idx==4):
-            # Grab 10 instead of 1 for S10
-            subj_no_2 = file_per_algorithm[idx2:idx2+2]
-        else:
-            # For participant S2 - S8 (Even subject)
-            subj_no_2 = file_per_algorithm[idx2:idx2+1]
-
-       
-
-        # No. of zeros required
-        N = 1
-
-        # using zfill() adding leading zero
-        lead_zero_1 = subj_no_1.zfill(N + len(subj_no_1))
-
-        if (idx==4 and subj_no_2=="10"):
-            # Don't add leading zero to 10 since there are already 2 digits
-            lead_zero_2 = subj_no_2
-        else:
-            lead_zero_2 = subj_no_2.zfill(N + len(subj_no_2))
-
-
-        # print result
-
-
-        if (idx==0):
-            file_per_algorithm = file_per_algorithm.replace(str(idx+1), lead_zero_1)
-            file_per_algorithm = file_per_algorithm.replace(str(idx+2), lead_zero_2)
-
-        elif (idx==1):
-
-            file_per_algorithm = file_per_algorithm.replace(str(idx+2), lead_zero_1)
-            file_per_algorithm = file_per_algorithm.replace(str(idx+3), lead_zero_2)
-
-        elif (idx==2):
-
-            file_per_algorithm = file_per_algorithm.replace(str(idx+3), lead_zero_1)
-            file_per_algorithm = file_per_algorithm.replace(str(idx+4), lead_zero_2)
+    for idx in range(0,len(files_to_rename),5):
+        if (idx == 0):
+            files_per_algorithm = files_to_rename[idx:idx+5]
+            # print(files_per_algorithm)
+        elif (idx == 5):
+            files_per_algorithm = files_to_rename[idx:idx+5]
+            # print(files_per_algorithm)
+        elif (idx == 10):
+            files_per_algorithm = files_to_rename[idx:idx+5]
+            # print(files_per_algorithm)
         
-        elif (idx==3):
+        
+        for idx, file_per_algorithm in enumerate(files_per_algorithm):
+            if (idx==0):
+                idx1 = file_per_algorithm.index(str(idx+1))
+                idx2 = file_per_algorithm.index(str(idx+2))
 
-            file_per_algorithm = file_per_algorithm.replace(str(idx+4), lead_zero_1)
-            file_per_algorithm = file_per_algorithm.replace(str(idx+5), lead_zero_2)
+            elif (idx==1):
+                idx1 = file_per_algorithm.index(str(idx+2))
+                idx2 = file_per_algorithm.index(str(idx+3))
 
-        elif (idx==4):
+            elif (idx==2):
+                idx1 = file_per_algorithm.index(str(idx+3))
+                idx2 = file_per_algorithm.index(str(idx+4))
+            
+            elif (idx==3):
+                idx1 = file_per_algorithm.index(str(idx+4))
+                idx2 = file_per_algorithm.index(str(idx+5))
+
+            elif (idx==4):
+                idx1 = file_per_algorithm.index(str(idx+5))
+                idx2 = file_per_algorithm.index(str(idx+6))
+
+
+            subj_no_1 = file_per_algorithm[idx1:idx1+1]
+            if (idx==4):
+                # Grab 10 instead of 1 for S10
+                subj_no_2 = file_per_algorithm[idx2:idx2+2]
+            else:
+                # For participant S2 - S8 (Even subject)
+                subj_no_2 = file_per_algorithm[idx2:idx2+1]
+
            
+            # using zfill() adding leading zero
+            lead_zero_1 = subj_no_1.zfill(N + len(subj_no_1))
 
-            file_per_algorithm = file_per_algorithm.replace(str(idx+5), lead_zero_1)
-            # new_no_2 = file_per_algorithm.replace(str(idx+6), lead_zero_2)
-        
-        print(file_per_algorithm)
+            if (idx==4 and subj_no_2=="10"):
+                # Don't add leading zero to 10 since there are already 2 digits
+                lead_zero_2 = subj_no_2
+            else:
+                lead_zero_2 = subj_no_2.zfill(N + len(subj_no_2))
 
 
-# Replace actual filename
+            # print result
+
+
+            if (idx==0):
+                file_per_algorithm = file_per_algorithm.replace(str(idx+1), lead_zero_1)
+                file_per_algorithm = file_per_algorithm.replace(str(idx+2), lead_zero_2)
+
+            elif (idx==1):
+
+                file_per_algorithm = file_per_algorithm.replace(str(idx+2), lead_zero_1)
+                file_per_algorithm = file_per_algorithm.replace(str(idx+3), lead_zero_2)
+
+            elif (idx==2):
+
+                file_per_algorithm = file_per_algorithm.replace(str(idx+3), lead_zero_1)
+                file_per_algorithm = file_per_algorithm.replace(str(idx+4), lead_zero_2)
+            
+            elif (idx==3):
+
+                file_per_algorithm = file_per_algorithm.replace(str(idx+4), lead_zero_1)
+                file_per_algorithm = file_per_algorithm.replace(str(idx+5), lead_zero_2)
+
+            elif (idx==4):
+            
+
+                file_per_algorithm = file_per_algorithm.replace(str(idx+5), lead_zero_1)
+                # new_no_2 = file_per_algorithm.replace(str(idx+6), lead_zero_2)
+            
+            # print(file_per_algorithm)
+            new_filenames.append(file_per_algorithm)
+
+    # Replace actual filename
+    for idx, old_file in enumerate(files_to_rename):
+        # Get full path of old file
+        old_filename_path = os.path.join(path, old_file)
+        # Get full path of new file
+        new_filename_path = os.path.join(path, new_filenames[idx])
+        # Rename actual filename
+        os.rename(old_filename_path, new_filename_path)
+
+    print("Leading zeroes have been added to subject file number 1 - 9")
        
     
 
 
-# %% [markdown]
-# ## Replace actual name still has an issue
-
 # %%
-# Still has an issue....
-## Replace actual file names
-os.chdir(directory)
-# dir_files2_replace = os.getcwd()
-for idx, old_file_name in enumerate(files_to_rename):
-    # old_file_name = os.getcwd()+"/"+val
-    os.rename(old_file_name, file_per_algorithm[idx])
-
-
-
-# %%
-files_to_rename
-
-# %% [markdown]
-# ## Making leading to zero adjusted for file name 
-#
-
-# %%
-############# This is to add leading zero ##########    
-# Replace S5 with S05
-
-file = 'Pre_ccorr_combined_pair_S5_and_S6_actual_score_data.pkl'    
-idx1 = file.index("5", 0)
-idx2 = file.index("6", 1)
-
-subj_no_1 = file[idx1:idx1+1]
-subj_no_2 = file[idx2:idx2+1]
-# print(subj_no_1)
-# print(subj_no_2)
-
-# No. of zeros required
-N = 1
-
-# using zfill() adding leading zero
-lead_zero_1 = subj_no_1.zfill(N + len(subj_no_1))
-
-# print result
-print("The string after adding leading zeros : " + str(lead_zero_1))
-
-new_file = file.replace("5", lead_zero_1)
-
-# new_file = file.replace("6", lead_zero_1)
-
-print(new_file)
-
-
-
-
-
-# %% [markdown]
-# ## Original sample of making leading to zero
-
-# %%
-# initializing string
-test_string = '5'
-
-# printing original string
-print("The original string : " + str(test_string))
-
-# No. of zeros required
-N = 1
-
-# using zfill()
-# adding leading zero
-res = test_string.zfill(N + len(test_string))
-
-# print result
-print("The string after adding leading zeros : " + str(res))
-
-# %%
-# TODO Find pattern in file name, eg. S5 and S6. DO looping for this.
-# TODO Get the index of that S5 and S6 or 5 / 6
-# TODO Replace S5/ S6 with subject name with leading zero
-#TODO Add filename with subject no. < 10 with 0 at the beginning, eg. S1 => S01 
-# TODO Replace old filename with new one, then rename actual name with the new one
-actual_score_files = []
-
-for file in files:
-    # actual_score_files.append(re.search(r"actual_score", file))
-    if "actual_score" and "plv" in file:
-        print(file)
-
-# %% [markdown]
-# ## Temporary
-
-# %%
-# Python3 code to demonstrate working of
-# Convert list of dictionaries to Dictionary Value list
-# Using loop
-from collections import defaultdict
-import numpy as np
-
-# initializing lists
-# list_temp = [{"Gfg" : 6},
-# 			{"Gfg" : 8},
-# 			{"Gfg" : 2},
-# 			{"Gfg" : 12},
-# 			{"Gfg" : 22}]
-
-list_temp =[]
-a = {"a" : 1}
-aa = {"a" : 2}
-b = {"b" : 1}
-bb = {"b" : 3}
-
-for i in range(4):
-    list_temp.append(a)
-    list_temp.append(aa)
-    list_temp.append(b)
-    list_temp.append(bb)
-
-# printing original list
-print("The original list : " + str(list_temp))
-
-# using loop to get dictionaries
-# defaultdict used to make default empty list
-# for each key
-res = defaultdict(list)
-for sub in list_temp:
-	for key in sub:
-		res[key].append(sub[key])
-	
-# printing result
-print("The extracted dictionary : " + str(dict(res)))
-
-average_a = np.mean(res["a"])
-average_b = np.mean(res["b"])
-
-print(f"Average score of a : {average_a}")
-print(f"Average score of b : {average_b}")
-
+# path = "/hpc/igum002/codes/Hyperscanning2-redesign/data/EEG/significant_connections/natural_pre_original"
+# N = 1
+# add_leading_zero(path,N)
